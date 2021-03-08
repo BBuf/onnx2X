12895bb44f8b408e3f8d8e49e4e42ee645e809ef
BBuf <1182563586@qq.com>
2021-03-08 22:01:22 +0800

    restruct gather op

--- onnx2pytorch/operations/gather.py
+++ onnx2pytorch/operations/gather.py
@@ -5,9 +5,7 @@ from torch import nn
 class Gather(nn.Module):
     def __init__(self, dim=0):
         self.dim = dim
-        self.selection = [slice(None) for _ in range(dim)]
         super().__init__()
 
-    def forward(self, input: torch.Tensor, indices: torch.Tensor):
-        selection = self.selection + [indices.to(torch.int64)]
-        return input.__getitem__(selection)
+    def forward(self, input: torch.Tensor, index: torch.Tensor):
+        return torch.gather(input, self.dim, index)
