c460a5960acac2a57f9e9c803e31350718a50058
BBuf <1182563586@qq.com>
2021-03-08 22:45:39 +0800

    restruct globalavgpool op

--- onnx2pytorch/operations/pooling.py
+++ onnx2pytorch/operations/pooling.py
@@ -3,7 +3,8 @@ from torch import nn
 
 
 class GlobalAveragePool(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
     def forward(self, input: torch.Tensor):
-        spatial_shape = input.ndimension() - 2
-        dim = tuple(range(spatial_shape, spatial_shape + 2))
-        return torch.mean(input, dim=dim, keepdim=True)
+        return self.avgpool(input)
